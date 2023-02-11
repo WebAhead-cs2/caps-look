4a90cf9f395ad1e8c1a0342037a9b045abf653d4
Saher Hanif <saher.ha97@gmail.com>
2023-02-11 19:04:14 +0200

    fixed the prettier error

--- src/pages/Absence/index.jsx
+++ src/pages/Absence/index.jsx
@@ -4,48 +4,43 @@ import PageContainer from '../../components/PageContainer'
 import SearchBar from '../../components/SearchBar'
 
 export default function Absence() {
-  const [list, setList] = React.useState([]); // list of absences useState
-  
+  const [list, setList] = React.useState([]) // list of absences useState
 
-const getAbsence = async ()=>{
-  try{
-  const data = await fetch('http://localhost:4000/absence')
-  const absences = await data.json()
-  absences.map((absence) =>{
-    if (absence.mandatory){
-      return absence.mandatory = "mandatory"
-    }else{
-      return absence.mandatory = "opitonal"
+  const getAbsence = async () => {
+    try {
+      const data = await fetch('http://localhost:4000/absence')
+      const absences = await data.json()
+      absences.map((absence) => {
+        if (absence.mandatory) {
+          return (absence.mandatory = 'mandatory')
+        } else {
+          return (absence.mandatory = 'opitonal')
+        }
+      })
+
+      setList(absences)
+    } catch {
+      setList([])
     }
-    
-  });
-
-  setList(absences)
-  
-  }catch{
-    setList([])
   }
-}
-React.useEffect(() => {
-  getAbsence();
-}, []);
+  React.useEffect(() => {
+    getAbsence()
+  }, [])
 
-
-const columns = [
-  { title: 'absence', dataIndex: 'absence_name' },
-  { title: 'site', dataIndex: 'site_name' },
-  { title: 'country', dataIndex: 'country_name' },
-  { title: 'start date', dataIndex: 'absence_start_date' },
-  { title: 'end date', dataIndex: 'absence_end_date' },
-  { title: 'mandatory/optional', dataIndex: 'mandatory' }
-]
-
+  const columns = [
+    { title: 'absence', dataIndex: 'absence_name' },
+    { title: 'site', dataIndex: 'site_name' },
+    { title: 'country', dataIndex: 'country_name' },
+    { title: 'start date', dataIndex: 'absence_start_date' },
+    { title: 'end date', dataIndex: 'absence_end_date' },
+    { title: 'mandatory/optional', dataIndex: 'mandatory' }
+  ]
 
   return (
     <div>
       <PageContainer name={'Absences'}>
-      <SearchBar/>
-      <ContentsTable source={list} columns={columns} />
+        <SearchBar />
+        <ContentsTable source={list} columns={columns} />
       </PageContainer>
     </div>
   )
